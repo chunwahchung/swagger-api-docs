2d5f98f3b06ffe94f69422806d1c4817885fb432
Chun-Wah Chung <chunwah@my.yorku.ca>
2021-07-09 20:58:28 -0400

    create book

--- routes/books.js
+++ routes/books.js
@@ -96,6 +96,28 @@ router.get("/:id", (req,res) => {
     res.send(book);
 });
 
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     summary: Add book to library
+ *     tags: [Books]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses: 
+ *       200: 
+ *         description: The book was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       500:
+ *        description: Server Error
+ */ 
 router.post("/", (req, res) => {
     try {
         const book = {
@@ -104,6 +126,8 @@ router.post("/", (req, res) => {
         }
 
         req.app.db.get("books").push(book).write();
+
+        res.send(book);
     } catch (error) {
         return res.status(500).send(error);
     }
